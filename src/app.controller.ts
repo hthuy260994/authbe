c37c5b9f58297e0396e37bdc8accf1687f5e0f5e
Huy Huynh <hthuy.dev@gmail.com>
2024-10-21 08:55:19 +0700

    guard

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthenticationGuard } from './auth/guards/auth.guard';
 
+@UseGuards(AuthenticationGuard)
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(@Req() req) {
+    return { message: 'Accessed Resource', userId: req.userId };
   }
 }
